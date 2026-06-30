2aca3d91bde41a8a4c42fc9475429aa56286dce6
Fabian Alfaro <133072547+Alfxro@users.noreply.github.com>
2026-06-30 17:49:24 -0600

    cambio de config de api

--- services/api-config.ts
+++ services/api-config.ts
@@ -6,7 +6,7 @@
 // ============================================================
 
 export const API_URL =
-  process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000';
+  process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8080';
 
 const SESSION_KEY = 'sgid_session';
 
